0343a7b05fff2cdbda045f7f626e46395ea73aba
patdahl <pdahl615@gmail.com>
2023-08-10 09:37:44 -0500

    Update apps.py

--- app_name/apps.py
+++ app_name/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class {{camel_case_app_name}}(AppConfig):
+class {{camel_case_app_name}}AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = '{{app_name}}'
