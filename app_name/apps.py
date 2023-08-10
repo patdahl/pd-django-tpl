7edfd9a0292a1535a779a6889681f0864d6fb36c
patdahl <pdahl615@gmail.com>
2023-08-10 09:32:58 -0500

    Update and rename apps.py to apps.py

--- app_name/apps.py
+++ app_name/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MainConfig(AppConfig):
+class {{camel_case_app_name}}(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main'
+    name = '{{app_name}}'
