3c859be2d78f2e689847f0b5dcc83842a0216753
patdahl <pdahl615@gmail.com>
2023-09-11 08:49:41 -0500

    Update settings.py
    Added secret_key context slot to template.

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-998-uv^e=qj1_fmg2)5i*tw!1ccygu*6b16d&3e=x=i3gs-alp'
+SECRET_KEY = '{secret_key}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
